b12cf2b99b60ee917e5b9564b5a625ced2797b53
Glenn Jahnke <gleenn@apple.com>
2019-06-15 14:29:22 -0700

    working cassaforte example

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject cassaforte-playground "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clojurewerkz/cassaforte "3.0.0-alpha2-SNAPSHOT"]]
   :repl-options {:init-ns cassaforte-playground.core}
   :main cassaforte-playground.core/main)
--- src/cassaforte_playground/core.clj
+++ src/cassaforte_playground/core.clj
@@ -1,5 +1,58 @@
 (ns cassaforte-playground.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojurewerkz.cassaforte.client :as client]
+            [clojurewerkz.cassaforte.cql :as cql]
+            [clojurewerkz.cassaforte.query.dsl :as dsl]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;             MUST USE CASSANDRA 2.2!!!!!!!!           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def keyspace "glenn")
+
+(def session (doto (client/connect ["127.0.0.1"] {:keyspace         keyspace
+                                                  :protocol-version 2})
+               (cql/use-keyspace keyspace)))
 
 (defn main [& x]
   (println (str/join " " x)))
+
+(try (cql/drop-keyspace
+      session
+      keyspace)
+     (catch Exception e
+       (prn (:cause e))))
+
+(try (cql/create-keyspace
+      session keyspace
+      (dsl/with {:replication
+                 {"class"              "SimpleStrategy"
+                  "replication_factor" 1}}))
+     (catch Exception e
+       (prn (:cause e))))
+
+(try (cql/drop-table
+      session
+      :users)
+     (catch Exception e
+       (prn (:cause e))))
+
+(try (cql/create-table
+      session
+      :users
+      (dsl/column-definitions {:name        :varchar
+                               :age         :int
+                               :city        :varchar
+                               :primary-key [:name]}))
+     (catch Exception e
+       (prn (:cause e))))
+
+
+(cql/insert session
+            :users
+            {:name "Alex"
+             :city "Munich"
+             :age  (int 19)})
+
+(prn (cql/select session
+                 :users))
