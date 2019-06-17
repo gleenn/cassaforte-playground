c080e76e1751faf1f8b7ad3a22a0ec0db9459fbe
Glenn Jahnke <gleenn@apple.com>
2019-06-17 11:59:54 -0700

    add some printlns for debugging

--- src/cassaforte_playground/core.clj
+++ src/cassaforte_playground/core.clj
@@ -20,22 +20,25 @@
 (try (cql/drop-keyspace
       session
       keyspace)
+     (println "Dropped keyspace" keyspace)
      (catch Exception e
-       (prn (:cause e))))
+       (prn e)))
 
 (try (cql/create-keyspace
       session keyspace
       (dsl/with {:replication
                  {"class"              "SimpleStrategy"
                   "replication_factor" 3}}))
+     (println "Created keyspace" keyspace)
      (catch Exception e
-       (prn (:cause e))))
+       (prn e)))
 
 (try (cql/drop-table
       session
       :users)
+     (println "Dropped table" :users)
      (catch Exception e
-       (prn (:cause e))))
+       (prn e)))
 
 (try (cql/create-table
       session
@@ -45,9 +48,19 @@
                                :city        :varchar
 
                                :primary-key [:name]}))
+     (println "Created table" :users)
      (catch Exception e
-       (prn (:cause e))))
+       (prn e)))
 
+(try (cql/create-index
+      session
+      :users
+      :city
+      (dsl/index-name :users_city)
+      (dsl/if-not-exists))
+     (println "Created index" :users :city)
+     (catch Exception e
+       (prn e)))
 
 (cql/insert session
             :users
@@ -58,15 +71,15 @@
                   [{:name "Glenn" :city "San Francisco" :age (int 13)}
                    {:name "Kristen" :city "San Francisco" :age (int 12)}])
 
-(prn (cql/select session :users))
-
-(prn (cql/select session :users (dsl/where [[= :name "Alex"]])))
-;(prn (cql/select session :users (dsl/where [[= :city "San Francisco"]]))) ; Predicates on non-primary-key columns (city) are not yet supported for non secondary index queries
+;(prn (cql/select session :users))
+;(prn (cql/select session :users (dsl/where [[= :name "Alex"]])))
+; this will throw if you don't create the secondary index: Predicates on non-primary-key columns (city) are not yet supported for non secondary index queries
+(prn (cql/select session :users (dsl/where [[= :city "San Francisco"]])))
 
 
 ;; This takes ~250-340ms to insert 10K rows! :D
 #_(time
- (->> (range 10000)
-      (map #(hash-map :name (str "User" %) :city "San Francisco" :age (int (mod % 100))))
-      (partition-all 1000)
-      (#(doseq [users %] (cql/insert-batch session :users users)))))
+   (->> (range 10000)
+        (map #(hash-map :name (str "User" %) :city "San Francisco" :age (int (mod % 100))))
+        (partition-all 1000)
+        (#(doseq [users %] (cql/insert-batch session :users users)))))
